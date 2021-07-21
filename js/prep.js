02e8f9fb485e2fba952bde9ab8ab6a4316ba82b2
Clyde Anderson <clyde.anderson@dynatrace.com>
2021-07-21 18:18:21 -0400

    moved tabs control over to tabs.js

--- js/prep.js
+++ js/prep.js
@@ -21,34 +21,6 @@ chrome.storage.sync.get(['favorites'], (data) => {
     }
 });
 
-function changeTab(evt, tab) {
-    console.log('click fired on ' + tab);
-
-    //show the calling div
-    //favbutton = menu div
-    //expandedbutton = sections div
-    let caller = document.getElementById(tab);
-    caller.style.display = 'block';
-
-    //hide the other div
-    let other = tab === 'menu' ? document.getElementById('sections') : document.getElementById('menu');
-    other.style.display = 'none';
-
-    //adjust colors of buttons to show selected
-    if (tab === 'menu') {
-        document.getElementById('favbutton').style.backgroundColor = '#0075ff';
-        document.getElementById('favbutton').style.color = 'white';
-        document.getElementById('expandbutton').style.backgroundColor = '#ccc';
-        document.getElementById('expandbutton').style.color = 'black';
-    }
-    else {
-        document.getElementById('expandbutton').style.backgroundColor = '#0075ff';
-        document.getElementById('expandbutton').style.color = 'white';
-        document.getElementById('favbutton').style.backgroundColor = '#ccc';
-        document.getElementById('favbutton').style.color = 'black';
-    }
-}
-
 //prep the tab buttons
 document.getElementById('favbutton').addEventListener('click', () => {changeTab(event, 'menu');});
 document.getElementById('expandbutton').addEventListener('click', () => {changeTab(event, 'sections');});
